28307ce1741ff505be96e47a2e87eb715001cbed
mhussainshah1 <muhammadshah.14@sunymaritime.edu>
2019-03-28 06:14:08 -0400

    fixed-child table showing id

--- src/main/java/com/example/demo/DirectorRepository.java
+++ src/main/java/com/example/demo/DirectorRepository.java
@@ -3,5 +3,4 @@ package com.example.demo;
 import org.springframework.data.repository.CrudRepository;
 
 public interface DirectorRepository extends CrudRepository<Director, Long> {
-    Director findByName(String name);
 }
